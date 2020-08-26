996137095d44c74a30fc77c76c03df416510208e
jaymzee <jaymzee@users.noreply.github.com>
2020-08-26 09:05:55 -0400

    new file asm/HelloWorld

---
+++ asm/HelloWorld/hello.asm
@@ -0,0 +1,29 @@
+;Hello World Win64
+;Display Hello World in a MessageBox
+
+includelib kernel32.lib
+includelib user32.lib
+
+extrn MessageBoxA: PROC
+extrn ExitProcess: PROC
+
+.data
+caption db 'Win64', 0
+msg db 'Hello World', 0
+
+.code
+main proc
+	push	rbp
+	mov	rbp,rsp
+	sub	rsp,20h
+	mov	rcx,0		; hWnd = HWND_DESKTOP
+	lea	rdx,msg		; LPCSTR lpText
+	lea	r8,caption		; LPCSTR lpCaption
+	mov	r9d,0		; uType = MB_OK
+	call	MessageBoxA
+	mov	ecx,eax		; uExitCode = MessageBox(...)
+	call	ExitProcess
+	leave
+main endp
+
+END
