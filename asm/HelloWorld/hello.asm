80afbdfa6cc6446a1f12039aa25f887cea198aed
jaymzee <jaymzee@users.noreply.github.com>
2020-08-26 09:24:20 -0400

    cleanup hello.asm

--- asm/HelloWorld/hello.asm
+++ asm/HelloWorld/hello.asm
@@ -1,5 +1,5 @@
-;Hello World Win64
-;Display Hello World in a MessageBox
+; Hello World Win64
+; Display Hello World in a MessageBox
 
 includelib kernel32.lib
 includelib user32.lib
@@ -18,7 +18,7 @@ main proc
 	sub	rsp,20h
 	mov	rcx,0		; hWnd = HWND_DESKTOP
 	lea	rdx,msg		; LPCSTR lpText
-	lea	r8,caption		; LPCSTR lpCaption
+	lea	r8,caption	; LPCSTR lpCaption
 	mov	r9d,0		; uType = MB_OK
 	call	MessageBoxA
 	mov	ecx,eax		; uExitCode = MessageBox(...)
